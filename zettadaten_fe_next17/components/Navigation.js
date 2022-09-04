262d8e7be8a7ff4099b8b1a783ef5f2cf4a6f340
cadentic <55697529+cadentic@users.noreply.github.com>
2022-09-04 14:06:34 +0530

    modified:   zettadaten_fe_next17/components/Navigation.js 	modified:   zettadaten_fe_next17/package-lock.json 	modified:   zettadaten_fe_next17/package.json 	modified:   zettadaten_fe_next17/public/favicon.ico 	new file:   zettadaten_fe_next17/public/logo1.png 	modified:   zettadaten_fe_next17/yarn.lock

--- zettadaten_fe_next17/components/Navigation.js
+++ zettadaten_fe_next17/components/Navigation.js
@@ -3,26 +3,144 @@ import PropTypes from 'prop-types';
 import IconButton from '@mui/material/IconButton';
 //import Grid from '@mui/material/Grid'; // Grid version 1
 import Grid from '@mui/material/Unstable_Grid2'; 
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+import { styled, alpha } from '@mui/material/styles';
 //import Image from '@mui/material/Image';
-import { Container, Toolbar, Typography, CssBaseline } from '@mui/material';
+import { Container, Toolbar, Typography, CssBaseline, Paper, Box, AppBar } from '@mui/material';
 import MuiLink from '@mui/material/Link';
-//import useScrollTrigger from '@mui/material/useScrollTrigger';
-import { AppBar } from '@mui/material';
+import Badge from '@mui/material/Badge';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MailIcon from '@mui/icons-material/Mail';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import { grey, brown } from '@mui/material/colors';
+import  Image  from 'next/image';
+
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will defau
 
 const Navigations = () => {
   //const brown = brown[900];
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="error">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          size="large"
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
+
+
   return (
     <React.Fragment>
       <CssBaseline />
       
-      <AppBar position="sticky" bgcolor="brown[A900]" sx={{ top: 'auto', bottom: 2 }}>
-              <Toolbar>
+      <AppBar position="sticky" bgcolor="secondery.accent" sx={{ top: 'auto', bottom: 2 }}>
+        <Toolbar>
+          <Image src="/favicon.ico" alt="lol" width="160" height="16"/>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+
           <Typography variant="h6" gutterBottom component="div" sx={{ p: 2, pb: 0, top: 'auto', bottom: 2 }}>
             <MuiLink color="inherit" href="/">
               Home
