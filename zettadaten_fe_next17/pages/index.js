3d00855773b57365ffa9195f5a39c89277e1f193
cadentic <55697529+cadentic@users.noreply.github.com>
2022-09-13 12:29:44 +0530

    modified:   zettadaten_fe_next17/pages/index.js

--- zettadaten_fe_next17/pages/index.js
+++ zettadaten_fe_next17/pages/index.js
@@ -116,6 +116,35 @@ Home.getInitialProps = async ctx => {
   } catch (error) {
     return { error };
   }
+/*
+  let one =
+  "https://api.storyblok.com/v1/cdn/stories/health?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt";
+let two =
+  "https://api.storyblok.com/v1/cdn/datasources/?token=wANpEQEsMYGOwLxwXQ76Ggtt";
+let three =
+  "https://api.storyblok.com/v1/cdn/stories/vue?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt";
+
+const requestOne = axios.get(one);
+const requestTwo = axios.get(two);
+const requestThree = axios.get(three);
+
+axios
+  .all([requestOne, requestTwo, requestThree])
+  .then(
+    axios.spread((...responses) => {
+      const responseOne = responses[0];
+      const responseTwo = responses[1];
+      const responesThree = responses[2];
+
+      // use/access the results
+      console.log(responseOne, responseTwo, responesThree);
+    })
+  )
+  .catch(errors => {
+    // react on errors.
+    console.error(errors);
+  });
+*/
 };
 
 export default Home;
