7cf6ed7c8f036143c70869036dafc5b8377f039f
cadentic <55697529+cadentic@users.noreply.github.com>
2022-09-19 23:26:19 +0530

    modified:   zettadaten_fe_next17/components/CopyerightZettadaten.js

--- zettadaten_fe_next17/components/CopyerightZettadaten.js
+++ zettadaten_fe_next17/components/CopyerightZettadaten.js
@@ -1,7 +1,7 @@
 //import Link from 'next/link'
 import React from 'react'
 import Copyright from '../src/Copyright';
-import { Container, Typography, Paper,Box , Card, Stack} from "@mui/material";
+import { Container, Typography, Paper, Box, Card, Stack, Divider } from "@mui/material";
 import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
 import MuiLink from '@mui/material/Link';
 
@@ -9,20 +9,20 @@ const CopyerightZettadaten = () => {
   return (
 
     <React.Fragment>
-      <Stack spacing={1} sx={{ bgcolor: '#3a3832', color: '#fff' }}>
-      <Box variant="outlined" square={true} sx={{ p: 10, flexGrow: 1, width: '100%', bgcolor: '#3a3832', color: '#fff', border: 1,borderRadius: 1 }}>
+      {/*<Stack spacing={1} sx={{ bgcolor: '#3a3832', color: '#fff' }} divider={<Divider orientation="vertical" flexItem />}> */}
+        <Box variant="outlined" square={true} divider={<Divider orientation="vertical" flexItem />} sx={{ p: 10, flexGrow: 1, width: '100%', bgcolor: '#3a3832', color: '#fff', border: 1,borderRadius: 1 }}>
       <Grid2 container spacing={3}>
           <Grid2 xs={6}>
-            <item>xs=6</item>
+            <item>xs=6 get items from loop</item>
         </Grid2>
         <Grid2 xs>
-          <item>xs=Auto</item>
+            <item>xs=Auto get items from loop</item>
           </Grid2>
           <Grid2 xs>
-            <item>xs=Auto</item>
+            <item>xs=Autoget items from loop</item>
           </Grid2>
           <Grid2 xs>
-            <item>xs=Auto</item>
+            <item>xs=Auto get items from loop</item>
           </Grid2>
           </Grid2>
         </Box>
@@ -30,7 +30,7 @@ const CopyerightZettadaten = () => {
       <Paper>
       <Copyright />
       </Paper>
-      </Stack>
+{/*      </Stack> */}
     </ React.Fragment>
 
 
