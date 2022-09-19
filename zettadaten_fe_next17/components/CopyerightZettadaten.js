996f4c73dcb264dbc29e3628c2f1d28358aedc1c
cadentic <55697529+cadentic@users.noreply.github.com>
2022-09-19 23:01:11 +0530

    modified:   zettadaten_fe_next17/components/CopyerightZettadaten.js

--- zettadaten_fe_next17/components/CopyerightZettadaten.js
+++ zettadaten_fe_next17/components/CopyerightZettadaten.js
@@ -1,16 +1,36 @@
 //import Link from 'next/link'
 import React from 'react'
 import Copyright from '../src/Copyright';
-//import { Container, Typography, Paper } from "@mui/material";
+import { Container, Typography, Paper,Box,Item , Card} from "@mui/material";
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import MuiLink from '@mui/material/Link';
 
 const CopyerightZettadaten = () => {
   return (
 
-    <div>
+    <React.Fragment>
+      <Box variant="outlined" square={true} sx={{ p: 10, flexGrow: 1, width: '100%', bgcolor: '#3a3832', color: '#fff', border: 1,borderRadius: 1 }}>
+      <Grid2 container spacing={3}>
+          <Grid2 xs={6}>
+            <item>xs=6</item>
+        </Grid2>
+        <Grid2 xs>
+          <item>xs=Auto</item>
+          </Grid2>
+          <Grid2 xs>
+            <item>xs=Auto</item>
+          </Grid2>
+          <Grid2 xs>
+            <item>xs=Auto</item>
+          </Grid2>
+          </Grid2>
+        </Box>
+        
+      <Paper>
       <Copyright />
+      </Paper>
 
-
-    </ div>
+    </ React.Fragment>
 
 
   )
