a693e65af359a2444ccb8d8e56a53c4d215f2e92
cadentic <55697529+cadentic@users.noreply.github.com>
2022-09-23 16:00:27 +0530

    modified:   ZETTADATEN/src/api/page/content-types/page/schema.json 	modified:   zettadaten_fe_next17/components/CopyerightZettadaten.js

--- zettadaten_fe_next17/components/CopyerightZettadaten.js
+++ zettadaten_fe_next17/components/CopyerightZettadaten.js
@@ -10,18 +10,22 @@ const CopyerightZettadaten = () => {
 
     <React.Fragment>
       {/*<Stack spacing={1} sx={{ bgcolor: '#3a3832', color: '#fff' }} divider={<Divider orientation="vertical" flexItem />}> */}
-        <Box variant="outlined" square={true} divider={<Divider orientation="vertical" flexItem />} sx={{ p: 10, flexGrow: 1, width: '100%', bgcolor: '#3a3832', color: '#fff', border: 1,borderRadius: 1 }}>
+      <Box variant="outlined" square={true} divider={<Divider orientation="vertical" flexItem sx={{color: '#faf8f7'}} />} sx={{ p: 10, flexGrow: 1, width: '100%', bgcolor: '#3a3832', color: '#fff', border: 1,borderRadius: 1 }}>
       <Grid2 container spacing={3}>
           <Grid2 xs={6}>
+            <item> Resources for </item>
             <item>xs=6 get items from loop</item>
         </Grid2>
-        <Grid2 xs>
+          <Grid2 xs>
+            <item> Why CADENTIC and CADENTIC ZETTAGIGA? </item>
             <item>xs=Auto get items from loop</item>
           </Grid2>
           <Grid2 xs>
+            <item> Looking For Something More ... </item>
             <item>xs=Autoget items from loop</item>
           </Grid2>
           <Grid2 xs>
+            <item> Try Something From Here ... </item>
             <item>xs=Auto get items from loop</item>
           </Grid2>
           </Grid2>
